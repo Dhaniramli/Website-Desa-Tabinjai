63b26d89cb41140af71fc02e373db4daae115fe7
Rahmadani Ramli <dani5087869@gmail.com>
2023-10-28 12:30:27 +0800

    perbaikan navbar

--- resources/views/user/layouts/partials/navbar.blade.php
+++ resources/views/user/layouts/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container">
         <div class="logo">
-            <a href="#">Desa Tabbinjai</a>
+            <a href="/">Desa Tabbinjai</a>
             <h6>Kec. Sinjai Barat Kab.Sinjai</h6>
         </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -11,7 +11,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link {{ Request::is('/*') ? 'active' : ''}}" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Profil Desa</a>
