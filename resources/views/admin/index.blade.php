2f913354ee9c9e915771f19ecbf32c628f381901
Rahmadani Ramli <dani5087869@gmail.com>
2023-10-30 11:22:37 +0800

    perbaikan

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,15 +1,17 @@
 @extends('admin.layouts.main')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid dashboard-1">
     <div class="row">
         <div class="col-md-12 grid-margin">
             <div class="row">
-                <div class="col-12 col-xl-8 mb-12 mb-xl-0">
-                    <h3 class="font-weight-bold">Selamat Datang</h3>
-                    <h6 class="font-weight-normal mb-0">Di Dashboard Desa Tabinjai
-                        {{-- <span class="text-primary">Sulawesi Selatan</span> --}}
-                    </h6>
+                <div class="card shadow p-5 dashboard-2">
+                    <div class="col-12 mx-auto my-auto">
+                        <h3 class="font-weight-bold text-center">Selamat Datang</h3>
+                        <h6 class="font-weight-normal mb-0 text-center"><span class="desanya">Di Dashboard Desa
+                                Tabbinjai</span>
+                        </h6>
+                    </div>
                 </div>
             </div>
         </div>
